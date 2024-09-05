c3a9f6f959ed2daf003a5e88407866c690217d23
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-05 07:21:28 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/package.sh)
-apt update && apt install $pack;
-echo "openbox --replace&" >> ~/.profile
+sudo apt update && sudo apt install -y $pack;
