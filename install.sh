4ab76124d9995de5256488fca45024917c40694a
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-05 07:24:18 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/package.sh)
 sudo apt update && sudo apt install -y $pack;
+cat <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/KLADE.desktop) > /use/share/xsessions/KLADE.desktop
