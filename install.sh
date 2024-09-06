8a36d492df481466951a776e4cba752534f334fb
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-06 12:10:50 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ source <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/pac
 sudo apt update && sudo apt install -y $pack;
 cat <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/KLADE.desktop) > /usr/share/xsessions/KLADE.desktop;
 cat <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/startKLADE) > /usr/bin/startKLADE;
-sudo chmod +x /use/bin/startKLADE
+sudo chmod +x /usr/bin/startKLADE
+sudo chmod +x /usr/share/xsessions/KLADE.desktop
